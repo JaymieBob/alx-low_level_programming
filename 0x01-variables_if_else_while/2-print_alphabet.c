ae1a25b76b3054d15160fa5e9d0e0f33e2b7e015
JaymieBob <bobbyfuse@yahoo.com>
2022-10-18 06:37:10 -0700

    New file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,10 @@ nclude <stdio.h>
 int main(void)
 {
 		char i;
-
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			putchar(i);
-		}
-		putchar('\n');
-		return (0);
+			for (i = 'a'; i <= 'z'; i++)
+			{
+				putchar(i);
+			}	
+					putchar('\n');
+						return (0);
 }
